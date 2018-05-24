b29a8f2ea289652ccacef30a568654a7384795a4
Sonia Gorla <soniagorla@msn.com>
2018-05-23 23:15:23 -0500

    Fix indentation

--- js/app.js
+++ js/app.js
@@ -86,16 +86,16 @@ function removeDeckShuffleClass() {
 
 //Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+  let currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
 }
 
 //Called if game is restart.
@@ -103,8 +103,8 @@ function shuffle(array) {
 function resetCards() {
   let openCardsArray = Array.prototype.slice.call(openCardsHC);
   openCardsArray.forEach(function(card) {
-      card.classList.add('closed');
-      card.classList.remove('open', 'show');
+    card.classList.add('closed');
+    card.classList.remove('open', 'show');
   });
   let matchCardsArray = Array.prototype.slice.call(matchCardsHC);
   matchCardsArray.forEach(function(card) {
